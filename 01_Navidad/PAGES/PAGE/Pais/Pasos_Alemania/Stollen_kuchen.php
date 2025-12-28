be0ef6e9efcdd19244d6f31fe9f9711506a98839
Eduardo <151774991+eduu2005@users.noreply.github.com>
2025-12-28 20:53:33 +0100

    Receta Stollen terminada

--- 01_Navidad/PAGES/PAGE/Pais/Pasos_Alemania/Stollen_kuchen.php
+++ 01_Navidad/PAGES/PAGE/Pais/Pasos_Alemania/Stollen_kuchen.php
@@ -24,23 +24,26 @@ session_start();
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" href="/Proyectos_Edu_Priv/01_Navidad/index.php" aria-current="page">Inicio</a>
+                    <a class="nav-link active" href="/Proyectos_Edu_Priv/01_Navidad/index.php"
+                        aria-current="page">Inicio</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#" aria-current="page">Información</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/Proyectos_Edu_Priv/01_Navidad/PAGES/PAGE/recetas.php" aria-current="page">Recetas</a>
+                    <a class="nav-link" href="/Proyectos_Edu_Priv/01_Navidad/PAGES/PAGE/recetas.php"
+                        aria-current="page">Recetas</a>
                 </li>
             </ul>
             <div class="d-flex">
                 <?php if (isset($_SESSION['id_usuario'])): ?>
-                    <div class="d-flex align-items-center">
-                        <span class="text-white me-2">Sesión iniciada: <strong><?php echo htmlspecialchars($_SESSION['nombre_usuario']); ?></strong></span>
-                        <a href="../../PHP/index.php" class="btn btn-light btn-sm ms-2">Mi cuenta</a>
-                    </div>
+                <div class="d-flex align-items-center">
+                    <span class="text-white me-2">Sesión iniciada:
+                        <strong><?php echo htmlspecialchars($_SESSION['nombre_usuario']); ?></strong></span>
+                    <a href="../../PHP/index.php" class="btn btn-light btn-sm ms-2">Mi cuenta</a>
+                </div>
                 <?php else: ?>
-                    <a href="../login.php" class="btn login-btn-custom">🎅 Iniciar Sesión</a>
+                <a href="../login.php" class="btn login-btn-custom">🎅 Iniciar Sesión</a>
                 <?php endif; ?>
             </div>
         </div>
@@ -59,13 +62,60 @@ session_start();
     <!-- Cards -->
     <div class="container mt-3">
         <div class="row">
-            <div class="col-sm-4 mb-3">
+            <div class="col-sm-10 mb-3">
                 <div class="card">
                     <img src="../../../../IMG/stollen_kuchen.jpg" class="card-img-top" alt="Receta1">
                     <div class="card-body">
-                        <h5 class="card-title">Stollen kuchen</h5>
-                        <p class="card-text">Esto es un pan dulce navideño, ahora seguiremos con sus pasos de elaboración</p>
-                        <a href="#" class="btn btn-danger">Pasos</a>
+                        <!-- Ingredientes -->
+                        <h5>Ingredientes </h5>
+                        <p><b>-</b> 430 gr. De harina</p>
+                        <p><b>-</b> 100-150 gr. De azúcar</p>
+                        <p><b>-</b> 200 gr. De queso crema</p>
+                        <p><b>-</b> 3 huevos</p>
+                        <p><b>-</b> 80 gr. De manteca pomada</p>
+                        <p><b>-</b> 21 gr. De levadura fresca</p>
+                        <p><b>-</b> 6 gr. De sal</p>
+                        <p><b>-</b> 1 cdta. De esencia de vainilla</p>
+                        <p><b>-</b> 1/2 cdta. De nuez moscada</p>
+                        <p><b>-</b> Ralladura de naranja</p>
+                        <p><b>-</b> 400 gr. De pasas de uva</p>
+                        <p><b>-</b> 100 gr. De arándanos</p>
+                        <p><b>-</b> Para decorar: glasé</p>
+
+                        <!-- Preparación -->
+                        <h5>Preparación:</h5>
+                        <p><b>1.</b> Comenzamos colocando en un bol los huevos, añadimos el queso crema, la manteca
+                            pomada, 100 gr de azúcar o 150 gr.
+                            Si lo quieren más dulce, 6 gr. De sal, la ralladura de naranja, la esencia de vainilla, la
+                            canela y la nuez moscada.
+                            Previo a lo antedicho maceramos las frutas con Ron unas 4 horas o toda la noche.
+                        </p>
+                        <p><b>2.</b> Integramos todos los ingredientes con la batidora con las varillas de amasado, no
+                            hay que amasar.
+                            Batimos unos 4 a 5 minutos tiene que quedar como una crema. Luego agregamos la harina
+                            tamizada, la levadura fresca y la pizca
+                            de sal e integramos nuevamente con todos los ingredientes y dejamos en reposo 30 minutos.
+                        </p>
+                        <p><b>3.</b> Luego agregamos las frutas escurridas sin el Ron y volvemos a batir. Y una vez todo
+                            integrado en un molde rectangular untamos con manteca y un poquito de harina y colocamos la
+                            masa,
+                            esparcimos bien con ayuda de una espátula, tapamos y dejamos leudar unas 3 horas
+                            aproximadamente.
+                            Pasado el tiempo llevamos a horno precalentado 180 a 190° unos 30 minutos o hasta ver que
+                            está dorado.
+                            Retiramos del horno dejamos 10 minutos en el molde y desmoldamos hasta que se enfríe por
+                            completo.
+                        </p>
+                        <p><b>4.</b> En una ollita calentamos mermelada de Damasco agregamos una cucharada de agua y
+                            luego
+                            pincelamos el stollen. En un bol ponemos 150 gr. De azúcar glass y una cucharada de jugo de
+                            limón y
+                            una cucharada de agua mezclamos bien hasta lograr una consistencia cremosa y la esparcimos
+                            por toda
+                            la parte de arriba del stollen.
+                        </p>
+
+                        <a href="../Alemania.php" class="btn btn-danger">Volver a recetas de Alemania</a>
                     </div>
                 </div>
             </div>
@@ -79,20 +129,24 @@ session_start();
     <!-- Fin contenido de la página -->
 
     <!-- Footer -->
-        <footer class="py-1 mt-3 footer-principal bg-danger">
-            <ul class="nav justify-content-center border-bottom pb-1 mb-1">
-                <li class="nav-item"><a href="../Proyectos_Edu_Priv/01_Navidad/index.php" class="nav-link px-2 py-0 text-body-secondary footer-link">Inicio</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 py-0 text-body-secondary footer-link">Información general</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 py-0 text-body-secondary footer-link">Sobre nosotros</a></li>
-            </ul>
+    <footer class="py-1 mt-3 footer-principal bg-danger">
+        <ul class="nav justify-content-center border-bottom pb-1 mb-1">
+            <li class="nav-item"><a href="../Proyectos_Edu_Priv/01_Navidad/index.php"
+                    class="nav-link px-2 py-0 text-body-secondary footer-link">Inicio</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 py-0 text-body-secondary footer-link">Información
+                    general</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 py-0 text-body-secondary footer-link">Sobre
+                    nosotros</a></li>
+        </ul>
 
-            <p class="text-center text-body-secondary mb-0 footer-texto bg-danger">© 2025 Navidad. Todos los derechos reservados.|
-                <a rel="license" href="https://creativecommons.org/licenses/by-nc/4.0/"
-                    class="text-decoration-none text-body-secondary">
-                    Licencia Creative Commons
-                </a>
-            </p>
-        </footer>
+        <p class="text-center text-body-secondary mb-0 footer-texto bg-danger">© 2025 Navidad. Todos los derechos
+            reservados.|
+            <a rel="license" href="https://creativecommons.org/licenses/by-nc/4.0/"
+                class="text-decoration-none text-body-secondary">
+                Licencia Creative Commons
+            </a>
+        </p>
+    </footer>
 
 </body>
 
